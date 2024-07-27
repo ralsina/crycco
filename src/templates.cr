a4cd7d2a1233df176f2c82b6f140e9fd1670ad04
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-27 20:41:45 -0300

    Support templates passed as files with/without .j2 extension

--- src/templates.cr
+++ src/templates.cr
@@ -6,10 +6,8 @@ module Templates
   Env.loader = Crinja::Loader::FileSystemLoader.new(["templates/", "."])
 
   def get(name : String) : Crinja::Template
-    begin
     Env.get_template(name)
-    rescue ex : Crinja::TemplateNotFoundError
-      Env.get_template(name + ".j2")
-    end
+  rescue ex : Crinja::TemplateNotFoundError
+    Env.get_template(name + ".j2")
   end
 end
