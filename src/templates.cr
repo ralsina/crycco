ada4771d6efb5c2daa0b030c787796b973934f40
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-27 20:46:05 -0300

    Support templates baked-in

--- src/templates.cr
+++ src/templates.cr
@@ -1,9 +1,18 @@
 require "crinja"
+require "crinja/loader/baked_file_loader"
+
+module MyBakedTemplateFileSystem
+  BakedFileSystem.load("../templates", __DIR__)
+end
 
 module Templates
   extend self
   Env = Crinja.new
-  Env.loader = Crinja::Loader::FileSystemLoader.new(["templates/", "."])
+
+  Env.loader = Crinja::Loader::ChoiceLoader.new([
+    Crinja::Loader::FileSystemLoader.new(["templates/", "."]),
+    Crinja::Loader::BakedFileLoader.new(MyBakedTemplateFileSystem),
+  ])
 
   def get(name : String) : Crinja::Template
     Env.get_template(name)
