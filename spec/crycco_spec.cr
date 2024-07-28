5430f5f9d61a849d69361d5d24ba7ed7cb1932d5
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-28 08:01:31 -0300

    Update tests

--- spec/crycco_spec.cr
+++ spec/crycco_spec.cr
@@ -1,5 +1,7 @@
 require "./spec_helper"
 
+Crycco.load_languages("#{__DIR__}/../languages.yml")
+
 describe Crycco do
   describe "parse" do
     it "should split code from comments" do
@@ -14,18 +16,22 @@ describe Crycco do
       doc = Crycco::Document.new "#{__DIR__}/fixtures/2.cr"
       doc.sections.size.should eq(3)
     end
+    pending "should split code from comments in literate style" do
+      doc = Crycco::Document.new "#{__DIR__}/fixtures/1.cr.md"
+      doc.sections.size.should eq(2)
+    end
   end
 
   describe "Section" do
     it "should convert docs to html" do
-      section = Crycco::Section.new
+      section = Crycco::Section.new Crycco::LANGUAGES[".cr"]
       section.docs = "This is a comment\nMore comment\n"
       section.docs_html.should eq("<p>This is a comment\nMore comment</p>\n")
     end
     it "should convert code to html" do
-      section = Crycco::Section.new
+      section = Crycco::Section.new Crycco::LANGUAGES[".cr"]
       section.code = "code\ncode\n"
-      section.code_html(Crycco::LANGUAGES[".cr"]).should eq("<pre><code class=\"crystal\">code\ncode\n</code></pre>")
+      section.code_html.strip.should eq("<pre class=\"code\"><code class=\"crystal\">code\ncode\n</code></pre>")
     end
   end
 end
