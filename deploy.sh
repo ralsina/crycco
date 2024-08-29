2581d5a236759a8110c92bf89fa5aa92bdc7b2c5
Roberto Alsina <roberto.alsina@gmail.com>
2024-08-29 12:16:13 -0300

    chore: deploy to rocky server

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -e
 ameba --fix || true
 make
 bin/crycco src/languages.yml src/*.cr --theme phd 
-rsync -rav docs/* ralsina@pinky:/data/websites/crycco.ralsina.me/
+rsync -rav docs/* ralsina@rocky:/home/ralsina/web/websites/crycco.ralsina.me/
