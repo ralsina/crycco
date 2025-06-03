0f166fb4ff2cf356b9d0cfe6d8fe2c346e8a895c
Roberto Alsina <roberto.alsina@gmail.com>
2025-06-03 12:22:32 -0300

    chore: AUR build

--- do_aur.sh
+++ do_aur.sh
@@ -4,8 +4,8 @@ set -e
 
 NAME=crycco
 
-#rm -rf aur-${NAME}
-#git clone ssh://aur@aur.archlinux.org/${NAME}.git aur-${NAME}
+rm -rf aur-${NAME}
+git clone ssh://aur@aur.archlinux.org/${NAME}.git aur-${NAME}
 sed "s/pkgver=.*/pkgver=$(shards version)/" -i aur-${NAME}/PKGBUILD
 sed "s/pkgrel=.*/pkgrel=1/" -i aur-${NAME}/PKGBUILD
 cd aur-${NAME}
